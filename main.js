b13c6e4dbfff980b4f3bf0cdbe071e2bd28a2386
Stefan Matting <stefan.matting@gmail.com>
2012-12-14 19:44:22 +0100

    refectored to model view

--- main.js
+++ main.js
@@ -28,7 +28,6 @@ function Bell() {
     }
 }
 
-var bell = new Bell();
 
 String.prototype.toMMSS = function () {
     var sec_numb    = parseInt(this);
@@ -62,7 +61,7 @@ function PhasedTimer(times) {
 
         this.lastTick = getTime();
         this.running = true;
-        //this.ontransition(0);
+        this.ontransition(0);
     }
 
     this.tick = function() {
@@ -99,113 +98,118 @@ function PhasedTimer(times) {
     }
 }
 
-function closeEdit(spanElt) {
-    var x = parseInt(spanElt.children("input").attr("value"));
-    if (!isNaN(x))
-        spanElt.children("label").html(x);
-    var nMinutes = parseInt(spanElt.children("label").html());
-    spanElt.children(".units").html(nMinutes <= 1 ? "minute." : "minutes.");
-    spanElt.removeClass("editing");
-}
-
-function highlightPhase(k) {
-    if (k < 4) {
-        $("h1").addClass("inactive");
-        $("h1").eq(k).removeClass("inactive");
-    } else {
-        $("h1").removeClass("inactive");
-    }
-}
-function updateProgress() {
-    $("#time-left-1").html((Math.floor(this.times[0] / 1000)).toString().toMMSS());
-    $("#time-left-2").html((Math.floor(this.times[2] / 1000)).toString().toMMSS());
-}
-
-function resetTimer(timer) {
-    var minutesPreperation = parseInt($("#label-1").html());
-    var minutesMeditation = parseInt($("#label-2").html());
-    //timer.reset([minutesPreperation*60*1000,2*1000,minutesMeditation*60*1000,2*1000]);
-    //timer.reset([2*1000,2*1000,2*1000,2*1000]);
-    //timer.reset([500,500,500,500]);
-}
-
-
-$(document).ready(function () {
-    $("#edit-1").click(function() {
-        $("#edit-1").attr("value", $("#label-1").html());
-        $("#span-1").addClass("editing");
-        $("#edit-1").select();
-    });
-    $("#edit-1").blur(function() {
-        closeEdit($("#span-1"));
-    });
-    $("#edit-1").keypress(function(e) {
-        if (e.keyCode == 13)
-            closeEdit($("#span-1"));
-    });
-    $("#label-1").focus(function(e) {
-        console.log("got focus!");
-    });
-
-    $("#edit-2").click(function() {
-        $("#edit-2").attr("value", $("#label-2").html());
-        $("#span-2").addClass("editing");
-        $("#edit-2").select();
-    });
-    $("#edit-2").blur(function() {
-        closeEdit($("#span-2"));
-    });
-    $("#edit-2").keypress(function(e) {
-        if (e.keyCode == 13)
-            closeEdit($("#span-2"));
-    });
-    
-
-
-
-    $("#bowl-hint").click(function() {
-        $("#bowl-hint").css("visibility", "hidden");
-        bell.mute();
-    });
-    
-    bell.onring = function() {
-        $("#stop-sound").css("visibility", "visible");
+function MinutesModel(id, minutes) {
+    if(minutes) {
+        this.minutes = minutes;
     }
 
-    bell.onend = function() {
-        $("#stop-sound").css("visibility", "hidden");
+    this.save = function(val) {
+        this.minutes = parseInt(val);
+        if(this.onChange)
+            this.onChange();
+    }
+}
+
+function MinutesView(model, el) {
+
+    this.update = function() {
+        var mins = this.model.minutes;
+        $(this.label).html(mins);
+        $(this.input).attr("value", mins);
+        if(mins > 1) {
+            $(this.units).html("minutes.");
+        } else {
+            $(this.units).html("minute.");
+        }
     }
 
-    $("#stop-sound").click(function() {
-        bell.mute();
-    });
+    this.commit = function() {
+        this.model.save($(this.input).attr("value"));
+    }
 
-    $("#bowl").click(function() {
-        bell.ring();
-    });
+    this.model = model;
+    this.el = el;
 
-    $("#bowl-wrap").hover(function() {
-        $("#bowl-hint").css("visibility", "visible");
-    },
-    function() {
-        $("#bowl-hint").css("visibility", "hidden");
-    });
+    this.silenceText = $("<span>Silence for</span>");
+    this.input = $("<input type='text' size='3' maxlength='3'>");
+    $(this.input).attr("id", $(this.el).attr("id") + "-input");
+    $(this.input).hide();
 
-
+    this.label = $("<label>");
+    $(this.label).attr("for", $(this.input).attr("id"));
 
 
+    this.units = $("<span>");
 
-    closeEdit($("#span-1"));
-    closeEdit($("#span-2"));
+    var that = this;
+    $(this.label).click(function() {
+            $(that.label).hide();
+            $(that.input).show();
+            $(that.input).focus();
+            return true;
+            });
+
+    $(this.input).blur(function() {
+            that.commit();
+            $(that.label).show();
+            $(that.input).hide();
+            });
+
+    $(this.input).focus(function() {
+            $(this).select();
+            });
+
+    $(this.input).keypress(function(e) {
+            if (e.keyCode == 13)
+            $(this).blur();
+            });
 
 
+    this.model.onChange = function() {
+        that.update();
+    }
+
+    $(this.el).append(this.silenceText);
+    $(this.el).append(this.label);
+    $(this.el).append(this.input);
+    $(this.el).append(this.units);
+}
+
+
+
+
+function AppView() {
+    var bell = new Bell;
     var timer = new PhasedTimer([1000,1000,1000,1000]);
+
+    var preparationModel = new MinutesModel("preparation-minutes", 1);
+    var preparationView = new MinutesView(preparationModel, $("#config-preparation"));
+    preparationView.update();
+
+    var meditationModel = new MinutesModel("meditation-minutes", 30);
+    var meditationView = new MinutesView(meditationModel, $("#config-meditation"));
+    meditationView.update();
+
+
+    function resetTimer() {
+        timer.reset([2*1000,2*1000,2*1000,2*1000]);
+    }
+
+    function highlight(k) {
+        if (0 <= k && k <= 3) {
+            $("#phases li").addClass("inactive");
+            $("#phases li").eq(k).removeClass("inactive");
+        } else {
+            $("#phases li").removeClass("incactive");
+        }
+    }
+
     var fresh = true;
 
     timer.ontick = updateProgress;
 
     timer.ontransition = function(k) {
-        highlightPhase(k);
+        highlight(k);
         if(k == 1 || k == 3)
             bell.ring();
     }
@@ -214,22 +218,70 @@ $(document).ready(function () {
         $("#right-column").removeClass("running");
         $("#timer-button").html("Start Timer");
         timer.times = [1000,1000,1000,1000];
-        highlightPhase(4);
+        highlight(-1);
         fresh = true;
     }
 
     $("#timer-button").click(function() {
-        if(timer.running) {
+            if(timer.running) {
             timer.pause();
             $("#timer-button").html("Resume Timer");
-        } else {
+            } else {
             fresh = false;
             timer.ontick();
             timer.run();
             $("#right-column").addClass("running");
             $("#timer-button").html("Pause Timer");
-        }
-    });
+            }
+            });
+}
 
-    resetTimer(timer);
+
+
+function updateProgress() {
+    $("#indicator-preparation").html((Math.floor(this.times[0] / 1000)).toString().toMMSS());
+    $("#indicator-meditation").html((Math.floor(this.times[2] / 1000)).toString().toMMSS());
+}
+
+$(document).ready(function () {
+        var app = new AppView();
+
+        //    
+        //
+        //
+        //
+        //    $("#bowl-hint").click(function() {
+        //        $("#bowl-hint").css("visibility", "hidden");
+        //        bell.mute();
+        //    });
+        //    
+        //    bell.onring = function() {
+        //        $("#stop-sound").css("visibility", "visible");
+        //    }
+        //
+        //    bell.onend = function() {
+        //        $("#stop-sound").css("visibility", "hidden");
+        //    }
+        //
+        //    $("#stop-sound").click(function() {
+        //        bell.mute();
+        //    });
+        //
+        //    $("#bowl").click(function() {
+        //        bell.ring();
+        //    });
+        //
+        //    $("#bowl-wrap").hover(function() {
+        //        $("#bowl-hint").css("visibility", "visible");
+        //    },
+        //    function() {
+        //        $("#bowl-hint").css("visibility", "hidden");
+        //    });
+        //
+        //
+        //
+        //
+        //    var timer = new PhasedTimer([1000,1000,1000,1000]);
+        //
+        //    resetTimer(timer);
 });
